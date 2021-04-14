667670da4e04d91aaca932ef1473e78d321635cc
chiru381 <chirukosanam123@gmail.com>
2021-04-14 14:37:16 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+dotenv.config({path:'./config/config.env'});
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res)=>{
+    res.send(`<h1>Cart Application</h1>`);
+});
+app.listen(process.env.PORT, (err)=>{
+    if(err) throw err;
+    console.log(`Server is running on Port No: ${process.env.PORT}`);
+});
