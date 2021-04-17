50b1b960172380295dd1c2b20eaec4df9fdd984e
chiru381 <chirukosanam123@gmail.com>
2021-04-17 09:46:08 +0530

    components

--- client/src/modules/products/components/Upload.js
+++ client/src/modules/products/components/Upload.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 let Upload=()=>{
+    let [product, setProduct]=useState({
+        productName: "",
+        price: "",
+        qty: "",
+        file: "",
+        avail: "",
+        desc: "",
+        usage: "",
+    });
+    let inputHandler=(event)=>{
+        setProduct({...product, [event.target.name]:event.target.value});
+    };
     return(
         <>
-        <h1>Upload Products</h1>
+        <section></section>
+        <section>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-8">
+                        <form>
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="ProductName" onChange={inputHandler} name="productName" />
+                            </div>
+                            <div className="form-group">
+                                <input type="number" className="form-control" placeholder="Price" onChange={inputHandler} name="price" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="QTY" onChange={inputHandler} name="qty" />
+                            </div>
+                            <div className="form-group">
+                                <input type="file" className="form-control" placeholder="Please Upload Image" onChange={inputHandler} name="image" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Available" onChange={inputHandler} name="avail" />
+                            </div>
+                            <div className="form-group">
+                                <textarea placeholder="Product Description" className="form-control" onChange={inputHandler} name="desc" ></textarea>
+                            </div>
+                            <div className="form-group">
+                                <textarea placeholder="Usage" className="form-control" onChange={inputHandler} name="usage" ></textarea>
+                            </div>
+                            <div className="form-group">
+                                <input type="submit" className="form-control btn btn-primary" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
--- client/src/modules/user/components/Signup.js
+++ client/src/modules/user/components/Signup.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 let Signup=()=>{
+    let [user, setUser]=useState({user: "", email: "", password: ""});
+    let inputHandler=(event)=>{
+        setUser({...user, [event.taget.name]:evant.target.value});
+    };
+    let submitHandler=(event)=>{
+        console.log(user);
+        event.preventDefault();
+    };
     return(
         <>
-        <h1>Signup Component</h1>
+        <section></section>
+        <section>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">
+                                <p className="display-6">Registration Details</p>
+                                <pre>{JSON.stringify(user)}</pre>
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={submitHandler}>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" name="user" placeholder="UserName" onChange={inputHandler} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" name="email" placeholder="Email" onChange={inputHandler} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" name="password" placeholder="Password" onChange={inputHandler} />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="submit" className="form-control btn btn-success" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
