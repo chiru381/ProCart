430f6b37de438ed8da8450fbccc17ec1dab43649
chiru381 <chirukosanam123@gmail.com>
2022-04-20 17:02:25 +0530

    ok

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -18,6 +18,7 @@ const sendEmail = async (email, subject, text) => {
             to: 'chiranjeevikosanam3@gmail.com',
             subject: 'Hi....',
             text: 'Hello...........',
+            html: `<h1>Welcome</h1><p>Your verification is successfully completed.</p>`
         });
 
         console.log("email sent sucessfully");
